e7207214c7c1b9d176235ab936a23d2b0e717724
abrar997 <abraralrawi997@gmail.com>
2021-12-16 23:41:59 +0300

    todolist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Link, Route, Routes } from "react-router-dom";
 import DarkMode from "./Component/DarkMode/DarkMode";
+import TodoList from './Component/TodoList/TodoList'
+import { Container } from "@mui/material";
 const App=()=>{
   return (
-    <>
-
+    <Container>
+      <Link to="/DarkMode">DarkMode</Link>
+      <Link to="/TodoList">TodoList</Link>
       <Routes>
-        <Route path="/DarkMode" element={<DarkMode />} />
+        <Route path="/DarkMode" element={<DarkMode />}/>
+        <Route path="/TodoList" element={<TodoList />} />
       </Routes>
-    </>
+    </Container>
   );
 }
 export default App;  
---
+++ src/Component/TodoList/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./TodoList.css";
+const TodoList = () => {
+    return (
+        <div>
+           todo list 
+        </div>
+    )
+}
+
+export default TodoList
