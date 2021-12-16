c164fe8d525941c23521f39edbd3225151955ddf
abrar997 <abraralrawi997@gmail.com>
2021-12-16 07:51:19 +0300

    dark mood

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </HashRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
